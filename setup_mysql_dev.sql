cd1aaded81fc2c7af826ec34fe243ce5be83ba41
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-04-05 02:36:18 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS electronic-store;
+CREATE DATABASE IF NOT EXISTS `electronic-store`;
 CREATE USER IF NOT EXISTS 'alx'@'localhost' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON `Eproject`.* TO 'alx'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'alx'@'localhost';
