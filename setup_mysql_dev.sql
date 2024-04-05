90ec3c47c6982ffe3b65be762fd01eb59eaebd8b
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2024-04-05 02:38:04 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 
 CREATE DATABASE IF NOT EXISTS `electronic-store`;
 CREATE USER IF NOT EXISTS 'alx'@'localhost' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON `Eproject`.* TO 'alx'@'localhost';
+GRANT ALL PRIVILEGES ON `electronic-store`.* TO 'alx'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'alx'@'localhost';
 FLUSH PRIVILEGES;
